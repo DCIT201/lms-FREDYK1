0f9a8a9ce8512a71626f3c2363a96f66d186a02a
FREDYK1 <fkwekukankam@gmail.com>
2024-12-09 23:24:35 +0000

    Create Book Class

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,37 @@
 public class Book {
 
-    // This is your working class.
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String gettitle(){
+        return title;
+    }
+
+    public String getauthor(){
+        return author;
+    }
+    
+    public int getyearPublished(){
+        return yearPublished;
+    }
+
+    public void settitle(String title){
+        this.title = title;
+    }
+
+    public void setauthor(String author){
+        this.author = author;
+    }
+
+    public void setyearPublished(int yearPublished){
+        this.yearPublished = yearPublished;
+    }
     
 }
