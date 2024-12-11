5d5d84feb335108981a7e118d5b827c3d552c7e2
FREDYK1 <fkwekukankam@gmail.com>
2024-12-11 05:45:59 +0000

    Fixing errors in work

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,37 +1,10 @@
-public class Book {
+package org.example;
 
-    private String title;
-    private String author;
-    private int yearPublished;
 
-    public Book(String title, String author, int yearPublished) {
-        this.title = title;
-        this.author = author;
-        this.yearPublished = yearPublished;
-    }
-
-    public String gettitle(){
-        return title;
-    }
-
-    public String getauthor(){
-        return author;
-    }
-    
-    public int getyearPublished(){
-        return yearPublished;
-    }
-
-    public void settitle(String title){
-        this.title = title;
-    }
-
-    public void setauthor(String author){
-        this.author = author;
-    }
-
-    public void setyearPublished(int yearPublished){
-        this.yearPublished = yearPublished;
-    }
-    
+/**
+ * @param yearPublished Add this field
+ */
+public record Book(String title, String author, int yearPublished) {
+    // Constructor
+    // Initialize the field
 }
